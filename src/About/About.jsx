0971a44ef42bc6dc0b4a4d47d8d856ae19468d95
Parvez Mohammed <parvezmohammad916@gmail.com>
2025-04-24 15:40:06 +0530

    first commit

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -56,10 +56,10 @@ function About() {
     <div>
       <div className="About">
         <div>
-          <motion.h2  initial="hidden" animate="visible" variants={h2variant}>Hey there! 👨‍💻✨</motion.h2>
+          <motion.h2 initial="hidden" animate="visible" variants={h2variant}>Hey there! 👨‍💻✨</motion.h2>
           <motion.p className="intro-para" variants={pvariant} initial="hidden" animate="show">
-            Welcome to my space where clean code meets clear purpose. I'm a React JS Developer who believes in building seamless user experiences, one component at a time. From thoughtful UI design to efficient state management, I’m always exploring new ways to grow and create.
-            If you're into clean code, cool tech, and constant learning—you’ll feel right at home here. 💻🚀🔥🎮🌿
+            Welcome to my space where clean code meets clear purpose. I'm a React JS Developer who believes in building seamless user experiences, one component at a time. From thoughtful UI design to efficient state management, I'm always exploring new ways to grow and create.
+            If you're into clean code, cool tech, and constant learning—you'll feel right at home here. 💻🚀🔥🎮🌿
           </motion.p>
         </div>
         <motion.div variants={imgvariant} initial="hidden" animate="show">
--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import './App.css';
-import React, { useContext } from "react";
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-import Navbar from './Navbar/Navbar'
-import History from './History/History'
-import Resume from './Resume/Resume'
-import Contact from './Contact/Contact'
-import Projects from './Projects/Projects'
-import Footer from './Footer/Footer'
-import About from './About/About'
-import { ThemeContext } from './Theme';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import About from './About/About';
+import History from './History/History';
+import Resume from './Resume/Resume';
+import Projects from './Projects/Projects';
+import Contact from './Contact/Contact';
+import Footer from './Footer/Footer';
 
 function App() {
-  const { theme } = useContext(ThemeContext);
   return (
     <Router>
-    <div className={`App ${theme}`}>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<About/>}/>  
-        <Route path='/History' element={<History/>}/>
-        <Route path='/Resume' element={<Resume/>}/>
-        <Route path='/Projects' element={<Projects/>}/>
-        <Route path='/Contact'element={<Contact/>}/>
-      </Routes>
-      <Footer/> 
-    </div>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<About/>}/>  
+          <Route path='/History' element={<History/>}/>
+          <Route path='/Resume' element={<Resume/>}/>
+          <Route path='/Projects' element={<Projects/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+        </Routes>
+        <Footer/> 
+      </div>
     </Router>
   );
 }
